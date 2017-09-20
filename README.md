effee3b4345ac69ec9804caf03a989e40b127429
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-20 06:46:18 -0700

    add README.md and update js code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # psychic-game
+
+1. Computer choose's random letter from array- done
+
+2. Lock in computers choosen letter so that its not a random letter each time.-done
+
+3. User input letter on keyboard and push to their array.- done
+
+4. If the user guesses the correct letter, game should restart and the wins should incraese by one and shown in the display
+
+5. If the user guesses incorrectly number of guess should increase by 1 until number of guesses reaches zero and the letter should be shown in the display.
+  5a. Remove letter so that user can't have any duplicates.
+
+6. when number of guesses reaches zero restart game, computer gets new guess, clear the array and guess goes back to starting point.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,15 +3,23 @@ var losses= 0;
 var numGuess= 9;
 var guessArr=[];
 
+  
 // computer picks random letter and can't be changed
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// List to validate userChoices is avaliable on keyboard
 var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// computer picks a number
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
  
+ // Trying to see if I can restart game by having computer pick a new number.
+
+// computer guess assigned to variable to use in function event.
 var computerFixedGuess = computerGuess
 
+// Runs through to to see if user gets the answer right or wrong
+  
 document.onkeyup = function(event){
   var userGuess= event.key;
   
@@ -19,22 +27,32 @@ document.onkeyup = function(event){
   if(userChoices.indexOf(userGuess) !== -1) {
     guessArr.push(event.key);
   }
+  // If condiitionalfor assigning points
+  if(userGuess !== computerFixedGuess && numGuess > 0)  {
+       numGuess--;
+      } 
+      else if (userGuess !== computerFixedGuess && numGuess === 0) 
+      {
+        losses++;
+        guessArr = [];
+        numGuess= 9;
+        // reset(i);
+       } 
+      else {
+        wins++;
+        guessArr=[];
+        numGuess= 9;
+        // reset(i);
+        }
 
-  if(userGuess !== computerFixedGuess && numGuess > 0) {
-    numGuess--;
-  } else if( userGuess !== computerFixedGuess && numGuess === 0) {
-    losses++;
-  } else {
-    wins++;
-  }
-
-  
-
+// console.log for debugging
 console.log(computerFixedGuess);
 
+// show on the screen to user
 document.getElementById("winsId").innerHTML = wins;
 document.getElementById("lossesId").innerHTML = losses;
 document.getElementById("guessesLeftId").innerHTML = numGuess;
 document.getElementById("guessesId").innerHTML = guessArr;
 
 };
+
